9068b45233514f9746b02cfdf8c47b2ffd8a6cd4
svensalyard <svensalyard@gmail.com>
2023-11-02 17:35:26 -0700

    Added Comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Grabs all elements to be manipulated in JS
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
@@ -6,19 +7,26 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 const timerContainer = document.getElementById('timer');
 const scoreContainer = document.getElementById('score-container');
 
+// Array to store saved scores
 const savedScores = [];
 
+// sets to a default of zero
 let shuffledQuestions, currentQuestionIndex, timer, timeInterval, score=0;
 
-
+// listens for when the game starts and calls startGame
 startButton.addEventListener('click', startGame);
+//Listens for when an answer is selected and calls select answer
 answerButtonsElement.addEventListener('click', selectAnswer);
+//Listens for when next button is clicked and calls show next question
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
     setNextQuestion();
 });
 
+// Starts game and sets timer to 30 secs and counts down and displays timer
+// ends game if timer ends
 function startGame() {
+    // clears previous intervals
     clearInterval(timeInterval);
     timer = 30;
     timerContainer.textContent = timer;
@@ -30,7 +38,9 @@ function startGame() {
             endGame();
         }
     }, 1000);
-
+    
+    //Hides start button and increases the randomisation of the questions
+    //shows questions and calls set next question
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
@@ -38,6 +48,7 @@ function startGame() {
     setNextQuestion();
 }
 
+//randomizes questions and ends game if all questions are answered
 function setNextQuestion() {
     resetState();
     if (currentQuestionIndex < shuffledQuestions.length) {
@@ -69,18 +80,22 @@ function resetState() {
 }
 
 function restartGame() {
+    // hides game over message
     const scoreContainer = document.getElementById('score-container');
     scoreContainer.classList.add('hide');
 
+    //calls start game function to start the next game
     startGame();
 }
 
-
+// saves whether you answered correctly
 function selectAnswer(e) {
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct === 'true';
     setStatusClass(selectedButton, correct);
 
+    // increases score by 5 if you answered correctly
+    // decreases time by 5 seconds if answered incorrectly
     if (correct) {
         score += 5;
     } else {
@@ -90,6 +105,7 @@ function selectAnswer(e) {
         }
     }
 
+    // disables buttons once question is answered to stop you from swapping once answers are revealed
     answerButtonsElement.querySelectorAll('button').forEach(button => {
         button.disabled = true;
     });
@@ -101,11 +117,12 @@ function selectAnswer(e) {
     if (currentQuestionIndex < shuffledQuestions.length - 1) {
         nextButton.classList.remove('hide');
     } else {
+        // if all questions are answered, handle the end of the game
         endGame();
     }
 }
 
-
+// shows correct and wrong answers by changing button background colors
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
@@ -115,22 +132,30 @@ function setStatusClass(element, correct) {
     }
 }
 
+// removes correct and wrong answer colors
 function clearStatusClass(element) {
     element.classList.remove('correct');
     element.classList.remove('wrong');
 }
 
+// Ends the game, stops the timer
 function endGame() {
     clearInterval(timeInterval);
 
+    //hides the questions
+    //shows the score container
     questionContainerElement.classList.add('hide');
     const scoreContainer = document.getElementById('score-container');
     scoreContainer.classList.remove('hide');
 
+    //increases score depending on how much time is left
     if (timer > 0) {
         score += timer;
     }
 
+    //shows a game over screen and displays your points
+    // lets you save score or restart game
+    // gives an input box to save initials
     scoreContainer.innerHTML = `
         <h2>Game Over!</h2>
         <p>Your Score: ${score} points</p> <!-- Use the total score here -->
@@ -141,44 +166,50 @@ function endGame() {
     `;
 }
 
-
+// Saves the score and displays it on the page
+// shows the leaderboard
 function saveScore() {
     const initialsInput = document.getElementById('initials');
     const initials = initialsInput.value;
 
+    //saves current scores to array
     const scoreData = {
-        initials: initials,
-        score: score
+        initials: initials, //take initals input
+        score: score    //take current score
     };
 
     savedScores.push(scoreData);
 
+    //sorts by highest score to lowest
     savedScores.sort((a, b) => b.score - a.score);
 
+    //updates leaderboard HTML
     const leaderboard = document.getElementById('leaderboard');
     leaderboard.classList.remove('hide');
 
     const scoreList = document.getElementById('score-list');
     scoreList.innerHTML = '';
 
+    //Takes the score data and displays it on a list element
     savedScores.forEach((scoreData, index) => {
         const listItem = document.createElement('li');
         listItem.textContent = `${index + 1}. ${scoreData.initials} - ${scoreData.score} points`;
         scoreList.appendChild(listItem);
     });
 
+    //clears initials input
     initialsInput.value = '';
 }
 
-
+// All of the questions and their states of true or false to store whether you answered correctly
 const questions = [
     {
         question: 'What is at the end of a line?',
         answers: [
-            { text: ';', correct: true },
-            { text: '.', correct: false },
-            { text: ':', correct: false },
-            { text: '?', correct: false }
+            { text: 'semi-colon', correct: true },
+            { text: 'period', correct: false },
+            { text: 'colon', correct: false },
+            { text: 'question mark', correct: false }
         ]
     },
     {
@@ -193,10 +224,10 @@ const questions = [
     {
         question: 'A ___ is used to call a function?',
         answers: [
-            { text: '{}', correct: false },
-            { text: '[]', correct: false },
-            { text: '()', correct: true },
-            { text: '<>', correct: false }
+            { text: 'Curly Brackets', correct: false },
+            { text: 'Brackets', correct: false },
+            { text: 'Parentheses', correct: true },
+            { text: 'Angle Brackets', correct: false }
         ]
     },
     {
